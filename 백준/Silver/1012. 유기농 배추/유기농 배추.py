b9b0ddc022f79c9e49fac56175764b1ef8242114
alsor6 <132369946+alsor6@users.noreply.github.com>
2023-08-19 05:21:15 +0900

    [Silver II] Title: 유기농 배추, Time: 300 ms, Memory: 31556 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,33 +1,36 @@
 import sys
-sys.setrecursionlimit(100000)
-def dfs(y,x):
-    if x < 0 or x >= m or y < 0 or y >= n:
+sys.setrecursionlimit(10**6)
+#변수 받기
+T = int(input())
+
+
+#함수부
+def bfs(y,x):
+    if y < 0 or y >= N or x <0 or x >= M:
         return False
     if ground[y][x] == 1:
         ground[y][x] = 0
-        dfs(y-1, x)
-        dfs(y, x-1)
-        dfs(y+1, x)
-        dfs(y, x+1)
+        bfs(y-1, x)
+        bfs(y, x-1)
+        bfs(y+1, x)
+        bfs(y, x+1)
         return True
     return False
 
-#test case
-t = int(input())
 
-#ground 생성
-for _ in range(t):
-    m,n,k = list(map(int, input().split()))
-    # 모두 0인 ground 생성
-    ground = [[0 for _ in range(m)] for _ in range(n)]
-    # 배추 심어진 곳에 1 표기
-    for _ in range(k):
-        m1, n1 = map(int, input().split())
-        ground[n1][m1] = 1
+#결과 실행
+for _ in range(T):
+    #ground 생성 및 배추 심기
+    M, N, K = map(int, input().split())
+    ground = [[0 for _ in range(M)]for _ in range(N)]
+    for _ in range(K) :
+        x, y = map(int, input().split())
+        ground[y][x] = 1
 
+    #지렁이 수 세기
     count = 0
-    for i in range(n):
-        for j in range(m):
-            if dfs(i,j) == True :
+    for i in range(N):
+        for j in range(M):
+            if bfs(i,j) == True :
                 count += 1
     print(count)
